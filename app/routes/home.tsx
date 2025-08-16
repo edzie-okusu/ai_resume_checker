2c57521062427b914e79e6fa994633047e82e701
Isaac Okusu <isaacokusu@gmail.com>
2025-08-16 04:03:47 +0100

    added auth.tsx.  auth broken. auth commented out. added filesuploads using react dropzone. reactdrop enables to access files uploads on pc. next commit will complete files upload technology

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,50 @@
+import Navbar from "~/components/navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "constants";
+import ResumeCard from "~/components/ResumeCard";
+import React, { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  //  const {isLoading, auth} = usePuterStore();
+
+  //   // enable user  access only when authenticated
+  //   const location = useLocation();
+  //   const navigate = useNavigate()
+
+  //   useEffect(() => {
+  //       if(!auth.isAuthenticated) navigate('/auth?next=/');
+  //   }, [auth.isAuthenticated])
+
+    
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover items-center bg-cover">
+      <Navbar />
+
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track Your Application & Resume Ratings</h1>
+          <h2>Review your adminssions and check AI-Powered feedback</h2>
+        </div>
+      </section>
+  
+      {resumes?.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+                <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+        
+    
+
+  </main>
 }
+ 
---
+++ app/routes/uploads.tsx
@@ -0,0 +1,63 @@
+import React, { useState, type FormEvent } from "react";
+import FilesUpload from "~/components/FilesUpload";
+import Navbar from "~/components/navbar";
+
+const Uploads = () => {
+    const [ isProcessing, setIsProcessing] = useState(false);
+    const [statusText, setStatusText] = useState('');
+
+    const handleSubmit = (e:FormEvent<HTMLFormElement>) => {}
+
+    return (
+
+        <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+            <Navbar />
+            <section className="main-section">
+                <div className="page-heading py-16">
+                    <h1>Smart feedback for your dream job</h1>
+
+                    {isProcessing ? (
+                        <>
+                            <h2>{statusText}</h2>
+                            <img src="/images/resume-scan.gif" className="w-full" />
+                        </>
+                    ): (
+                        <h2>Drop your resume for an ATS score and improvement tips</h2>
+                    )}
+
+                    {!isProcessing && (
+                        <form action="" id="upload-form" onSubmit={handleSubmit} className="mt-8 flex flex-col gap-4">
+                             <div className="form-div">
+                                <label htmlFor="company-name">Company Name</label>
+                             <input type="text" name="company-name" placeholder="company-name" />
+                            
+                             </div>
+                             
+                            <div className="form-div">
+                                <label htmlFor="job-title">Job Title</label>
+                             <input type="text" name="job-title" placeholder="job-title" />
+
+                            </div>
+                             
+                            <div className="form-div">
+                                <label htmlFor="job-description">Job Description</label>
+                             <textarea rows={5} name="job-description" placeholder="Job Description" />
+                                
+                            </div>
+
+                            <div className="form-div">
+                                <label htmlFor="uploader">Upload Resume</label>
+                                <FilesUpload /> 
+                            </div>
+
+                            <button className="primary-button" type="submit">Submit Resume</button>
+                             
+                        </form>
+                    )}
+                </div>
+            </section>
+        </main>
+    )
+}
+
+export default Uploads;
